cbd6769293346755011aebcf1cc0bbab2ed0c6ed
Ziga Mahne <ziga.mahne@arm.com>
2024-11-20 12:12:48 +0100

    Add initial pack contents

---
+++ Documents/OVERVIEW.md
@@ -0,0 +1,11 @@
+# NUCLEO-G474RE_BSP
+
+The **STMicroelectronics NUCLEO-G474RE Board Support Pack (BSP)**:
+
+- Contains examples in *csolution format* for usage with the [CMSIS-Toolbox](https://github.com/Open-CMSIS-Pack/cmsis-toolbox/blob/main/docs/README.md) and the  [VS Code CMSIS Solution](https://marketplace.visualstudio.com/items?itemName=Arm.cmsis-csolution) extension.
+- Requires the [Device Family Pack (DFP) for the STM32G4 series](https://www.keil.arm.com/packs/stm32g4xx_dfp-keil).
+- Is configured with [STM32CubeMX](https://www.st.com/en/development-tools/stm32cubemx.html) for the Arm Compiler 6 (MDK).
+
+## Content in *csolution format*
+
+- [Examples/Blinky](https://github.com/Open-CMSIS-Pack/ST_NUCLEO-G474RE_BSP/tree/main/Examples/Blinky) shows the basic usage of this board.
---
+++ Examples/Blinky/STM32CubeMX/NUCLEO-G474RE/MX_Device/MX_Device.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * File Name   : MX_Device.h
+ * Date        : 20/11/2024 10:46:05
+ * Description : STM32Cube MX parameter definitions
+ * Note        : This file is generated with a generator out of the
+ *               STM32CubeMX project and its generated files (DO NOT EDIT!)
+ ******************************************************************************/
+
+#ifndef MX_DEVICE_H__
+#define MX_DEVICE_H__
+
+/* MX_Device.h version */
+#define MX_DEVICE_VERSION                       0x01000000
+
+
+/*------------------------------ LPUART1        -----------------------------*/
+#define MX_LPUART1                              1
+
+/* Pins */
+
+/* LPUART1_RX */
+#define MX_LPUART1_RX_Pin                       PA3
+#define MX_LPUART1_RX_GPIO_Pin                  GPIO_PIN_3
+#define MX_LPUART1_RX_GPIOx                     GPIOA
+#define MX_LPUART1_RX_GPIO_Mode                 GPIO_MODE_AF_PP
+#define MX_LPUART1_RX_GPIO_PuPd                 GPIO_NOPULL
+#define MX_LPUART1_RX_GPIO_Speed                GPIO_SPEED_FREQ_LOW
+#define MX_LPUART1_RX_GPIO_AF                   GPIO_AF12_LPUART1
+
+/* LPUART1_TX */
+#define MX_LPUART1_TX_Pin                       PA2
+#define MX_LPUART1_TX_GPIO_Pin                  GPIO_PIN_2
+#define MX_LPUART1_TX_GPIOx                     GPIOA
+#define MX_LPUART1_TX_GPIO_Mode                 GPIO_MODE_AF_PP
+#define MX_LPUART1_TX_GPIO_PuPd                 GPIO_NOPULL
+#define MX_LPUART1_TX_GPIO_Speed                GPIO_SPEED_FREQ_LOW
+#define MX_LPUART1_TX_GPIO_AF                   GPIO_AF12_LPUART1
+
+#endif  /* MX_DEVICE_H__ */
